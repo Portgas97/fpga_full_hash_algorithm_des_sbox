2bbc71ecbc6f99aeb0d826241112a819957a0090
Francesco Venturini <34078295+Portgas97@users.noreply.github.com>
2022-05-12 13:22:56 +0200

    Update full_hash_algorithm_des_sbox_prototype.py

--- full_hash_algorithm_des_sbox_prototype.py
+++ full_hash_algorithm_des_sbox_prototype.py
@@ -1,15 +1,15 @@
 # Prototype for the project of Hardware and Embedded Security 2021/2022, Unipi
 # Candidates: Francesco Venturini & Pierfrancesco Bigliazzi
 # Professors: Sergio Saponara & Luca Crocetti
+from math import floor
 import string
-import random
 
 # ############################################################################ #
 # # # # # # # # # # # # # # # # #   COSTANTS   # # # # # # # # # # # # # # # # # 
 # ############################################################################ #
 
-# H initialization value
 H_init = [b'0100',b'1011',b'0111',b'0001',b'1101',b'1111',b'0000',b'0011']
+H_global = [8]
 
 # S-Box
 des_sbox = [
@@ -36,6 +36,32 @@ def ascii_to_binary(ascii_char):
     binary_output = bin(binascii)
     return binary_output
 
+# implements left rotation
+def rotl(num, bits):
+    bit = num & (1 << (bits-1))
+    num <<= 1
+    if(bit):
+        num |= 1
+    num &= (2**bits-1)
+    return num
+
+# TO CHECK
+def full_hash(H, msg_char):
+    H_tmp = [8]
+    sbox_value = compute_sbox(msg_char)
+    for r in range(4):
+        for i in range(8):
+            H_tmp[i] = H_global[(i+1) % 8] ^ sbox_value
+            rotl(H_tmp[i], floor(i/2))
+    H_global = H_tmp
+
+# TO CHECK
+def compute_sbox(msg_char):
+    row = 0b00 ^ (msg_char[0] + msg_char[7])
+    column = 0b0000 ^ (msg_char[1] + msg_char[2] + msg_char[3] + msg_char[4])
+    sbox_output = des_sbox[row][column]
+
+
 
 # ############################################################################ #
 # # # # # # # # # # # # # # # # # #   MAIN   # # # # # # # # # # # # # # # # # # 
@@ -49,22 +75,20 @@ print("alphabet:\n", alphabet)
 binary_alphabet = [ ascii_to_binary(a) for a in alphabet]
 print("binary_alphabet :\n", binary_alphabet)
 
-
 # message input
 print("Insert the message you want to hash:")
 msg = input()
 print("Message entered. Start hashing...")
-
 msg_list = list(msg)
-print(msg_list)
 len = len(msg_list)
 print("Length of the message: ", len)
 
+# hash computation 
+H_global = H_init
 for i in range(len):
     print("Character: ", msg_list[i])
+    full_hash(H_global, msg_list[i])
 
-# print('H vector')
-# for i in range(len(H_init_value))
-#     print(H_init_value[i])
+# TODO 
 
-# tentativo push
+
